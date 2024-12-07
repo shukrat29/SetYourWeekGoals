829ea6364bed02894ecf0ce9115a96425aaeb3ad
Shukra Tamang <shukratamang.npl@gmail.com>
2024-12-07 18:10:42 +1100

    show goals in table

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+let taskList = [];
+
 const handleOnSubmit = (e) => {
   const newForm = new FormData(e);
 
-  const goal = newForm.get("goal");
+  const task = newForm.get("task");
   const hour = newForm.get("hour");
-  console.log(goal, hour);
+
+  const obj = {
+    task,
+    hour,
+  };
+
+  taskList.push(obj);
+  displayEntryList();
+};
+
+const displayEntryList = () => {
+  let str = "";
+
+  const entryElm = document.getElementById("entryList");
+
+  taskList.map((item, i) => {
+    str += `<tr>
+                <td>${i + 1}</td>
+                <td>${item.task}</td>
+                <td>${item.hour}hr</td>
+                <td class="text-end">
+                  <button class="btn btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                  <button class="btn btn-success">
+                    <i class="fa-solid fa-arrow-right"></i>
+                  </button>
+                </td>
+              </tr>`;
+  });
+
+  entryElm.innerHTML = str;
 };
